2ee39db2bdfb5cf39d60068ea67a398f4af90212
coquid <61982168+coquid@users.noreply.github.com>
2021-07-25 15:36:46 +0900

    Ch5. Pawn and player controll

--- Source/ArenaBattle/ABPawn.cpp
+++ Source/ArenaBattle/ABPawn.cpp
@@ -9,6 +9,40 @@ AABPawn::AABPawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CAPSULE"));
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MESH"));
+	Movement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("MOVEMENT"));
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SPRINGARM"));
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("CAMERA"));
+
+	RootComponent = Capsule;
+	Mesh->SetupAttachment(Capsule);
+	SpringArm->SetupAttachment(Capsule);
+	Camera->SetupAttachment(SpringArm);
+
+	Capsule->SetCapsuleHalfHeight(88.0f);
+	Capsule->SetCapsuleRadius(34.0f);
+	Mesh->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -88.0f), FRotator(0.0f, -90.0f, 0.0f));
+	SpringArm->TargetArmLength = 400.0f;
+	SpringArm->SetRelativeRotation(FRotator(-15.0f, 0.0f, 0.0f));
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SK_CARDBOARD(TEXT("/Game/InfinityBladeWarriors/Character/CompleteCharacters/SK_CharM_Cardboard.SK_CharM_Cardboard"));
+	if (SK_CARDBOARD.Succeeded())
+	{
+		Mesh->SetSkeletalMesh(SK_CARDBOARD.Object);
+	}
+	else
+	{
+		ABLOG(Warning, TEXT("Fail to set skeletal mesh!"));
+	}
+
+	// Blueprint version animation 세팅
+	Mesh->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+	static ConstructorHelpers::FClassFinder<UAnimInstance> WARRIOR_ANIM(TEXT("/Game/Book/Animations/WarriorAnimBluePrint.WarriorAnimBluePrint.WarriorAnimBluePrint_C"));
+	if (WARRIOR_ANIM.Succeeded())
+	{
+		Mesh->SetAnimInstanceClass(WARRIOR_ANIM.Class);
+	}
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +50,14 @@ void AABPawn::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	//// Code version animation 세팅
+	//Mesh->SetAnimationMode(EAnimationMode::AnimationSingleNode);
+	//UAnimationAsset* AnimAsset = LoadObject<UAnimationAsset>(nullptr, TEXT("/Game/Book/Animations/WarriorRun.WarriorRun"));
+	//if (AnimAsset != nullptr)
+	//{
+	//	Mesh->PlayAnimation(AnimAsset, true);
+	//}
+
 }
 
 // Called every frame
@@ -30,6 +72,8 @@ void AABPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("UpDown"), this, &AABPawn::UpDown);
+	PlayerInputComponent->BindAxis(TEXT("LeftRight"), this, &AABPawn::LeftRight);
 }
 
 void AABPawn::PostInitializeComponents()
@@ -44,3 +88,14 @@ void AABPawn::PossessedBy(AController* NewController)
 	Super::PossessedBy(NewController);
 }
 
+void AABPawn::UpDown(float NewAxisAvlue)
+{
+	ABLOG(Warning, TEXT("UpDown : %f"), NewAxisAvlue);
+	AddMovementInput(GetActorForwardVector(), NewAxisAvlue); // RootComponent의 ForwardVector를 가져옴.
+}
+
+void AABPawn::LeftRight(float NewAxisAvlue)
+{
+	ABLOG(Warning, TEXT("LeftRight : %f"), NewAxisAvlue);
+	AddMovementInput(GetActorRightVector(), NewAxisAvlue); // RootComponent의 RightVector를 가져옴.
+}
