158ad7ad162a7e9e70212351e367e6f63a4ec269
coquid <61982168+coquid@users.noreply.github.com>
2021-07-29 08:03:42 +0900

    Ch8. Animation Montage

--- Source/ArenaBattle/ABAnimInstance.cpp
+++ Source/ArenaBattle/ABAnimInstance.cpp
@@ -33,4 +33,27 @@ void UABAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 void UABAnimInstance::PlayAttackMontage()
 {
 	Montage_Play(AttackMontage, 1.0f);
+	ABCHECK(Montage_IsPlaying(AttackMontage));
+}
+
+void UABAnimInstance::JumpToAttackMontage(int32 NewSection)
+{
+	ABCHECK(Montage_IsPlaying(AttackMontage));
+	Montage_JumpToSection(GetAttackMontageSectionName(NewSection), AttackMontage);
+}
+
+void UABAnimInstance::AnimNotify_AttackHitCheck()
+{
+	OnAttackHitCheck.Broadcast();
+}
+
+void UABAnimInstance::AnimNotify_NextAttackCheck()
+{
+	OnNextAttackCheck.Broadcast();
+}
+
+FName UABAnimInstance::GetAttackMontageSectionName(int32 Section)
+{
+	ABCHECK(FMath::IsWithinInclusive<int32>(Section, 1, 4), NAME_None);
+	return FName(*FString::Printf(TEXT("Attack%d"),Section));
 }
